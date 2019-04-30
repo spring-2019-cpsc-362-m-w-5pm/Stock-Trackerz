9c39dc23e27e1996db84852535ab3571520eb2ca
jsprchn2 <39418859+jsprchn2@users.noreply.github.com>
2019-04-29 19:37:13 -0700

    Initial Commit

---
+++ Stock_Trackerz_App/Stock_Trackerz_App/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Stock_Trackerz_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Search_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void AddToPortfolio_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void View_All_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void View_Portfolio_Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Stock_Info_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void SearchBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
